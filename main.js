c58254fb1d4e926d6eb17440414d959438ae493c
BluAxolotl <plush.gamer.456@gmail.com>
2021-06-13 15:07:22 +0000

    main.js edited online with Bitbucket

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 const print  = console.log
 
-var io = require('socket.io-client');
-
-var socket = io("http://localhost:3000/");
-
-socket.on('connect', () => {
-	socket.on('chat message', function(data){
-        print(data);
-	});
-})
+print("hello!")
