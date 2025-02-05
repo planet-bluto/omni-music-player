09229227287f035b3677655185fc3e2ac6853e33
Blu <plush.gamer.456@gmail.com>
2025-02-04 23:18:06 -0500

    bro I'm bout to go coco

--- ytmusic.py
+++ ytmusic.py
@@ -1,15 +1,11 @@
 import json
-from ytmusicapi import YTMusic
+from ytmusicapi import YTMusic, OAuthCredentials
 
 
 with open('ytclient.json', 'r') as f:
     ytclient = json.load(f)
 
-class OAuthCredentials:
-    client_id=ytclient["client_id"]
-    client_secret=ytclient["client_secret"]
-
-ytmusic = YTMusic('./oauth.json', oauth_credentials=OAuthCredentials())
+ytmusic = YTMusic('./oauth.json', oauth_credentials=OAuthCredentials(client_id=ytclient["client_id"], client_secret=ytclient["client_secret"]))
 import json
 
 library_songs = ytmusic.get_library_songs(limit=1000)
